56b1acdf083de9cc9bad64e8f393f58cb1089311
Vrglab <vrglab1@gmail.com>
2024-12-29 17:51:20 +0100

    Finished implementing the not gate logic

--- common/src/main/java/org/Vrglab/LogicGates/World/Blocks/BlockEntityBlocks/Customs/NotGateBlock.java
+++ common/src/main/java/org/Vrglab/LogicGates/World/Blocks/BlockEntityBlocks/Customs/NotGateBlock.java
@@ -3,6 +3,7 @@ package org.Vrglab.LogicGates.World.Blocks.BlockEntityBlocks.Customs;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
@@ -12,32 +13,42 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.properties.Property;
-import org.Vrglab.LogicGates.World.Blocks.BlockEntityTypes.LogicGateBlockEntityTypes;
-import org.Vrglab.LogicGates.World.WorldUtils;
-import org.Vrglab.Utils.Utils;
-import org.Vrglab.Utils.VLModInfo;
 import org.jetbrains.annotations.Nullable;
 
-public class NotGateBlock extends Block {
+public class NotGateBlock extends DirectionalBlock {
     public static final MapCodec<NotGateBlock> CODEC = simpleCodec(NotGateBlock::new);
     public static final IntegerProperty POWER = BlockStateProperties.POWER;
 
     @Override
-    protected MapCodec<? extends Block> codec() {
+    protected MapCodec<NotGateBlock> codec() {
         return CODEC;
     }
 
     public NotGateBlock(Properties properties) {
         super(properties);
-        this.registerDefaultState(((BlockState) getStateDefinition().any()).setValue(POWER, 0));
+        this.registerDefaultState(((BlockState) getStateDefinition().any()).setValue(POWER, 15));
+    }
+
+    @Override
+    public BlockState rotate(BlockState blockState, Rotation rotation) {
+        return (BlockState)blockState.setValue(FACING, rotation.rotate((Direction)blockState.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState blockState, Mirror mirror) {
+        return blockState.rotate(mirror.getRotation((Direction)blockState.getValue(FACING)));
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        return (BlockState)this.defaultBlockState().setValue(FACING, blockPlaceContext.getNearestLookingDirection().getOpposite().getOpposite());
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(POWER);
+        builder.add(new Property[]{POWER, FACING});
     }
 
     @Override
@@ -47,7 +58,7 @@ public class NotGateBlock extends Block {
 
     @Override
     public int getSignal(BlockState state, BlockGetter world, BlockPos pos, Direction direction) {
-        return direction != Direction.SOUTH ? state.getValue(POWER) : 0;
+        return (direction == state.getValue(FACING)) ? state.getValue(POWER) : 0;
     }
 
     @Override
@@ -59,21 +70,18 @@ public class NotGateBlock extends Block {
 
     private void updatePower(BlockState state, Level world, BlockPos pos) {
         boolean hasInputSignal = false;
-
-        for (Direction direction : Direction.values()) {
-            if (direction == Direction.SOUTH) {  // Ignore the south direction to prevent feedback loops
-                int inputSignal = getInputSignal(world, pos, direction);
-                if (inputSignal > 0) {
-                    hasInputSignal = true;
-                    break;
-                }
+        int inputSignal = getInputSignal(world, pos, state.getValue(FACING));
+        if (inputSignal > 0) {
+            BlockPos inputPos = pos.relative(state.getValue(FACING));
+            if (inputPos.equals(pos)) {
+                return;
             }
+            hasInputSignal = true;
         }
-
         int outputSignal = hasInputSignal ? 0 : 15;
-
         if (state.getValue(POWER) != outputSignal) {
-            world.setBlock(pos, state.setValue(POWER, outputSignal), 3);
+            BlockState newState = state.setValue(POWER, outputSignal);
+            world.setBlock(pos, newState, 3);
         }
     }
 
