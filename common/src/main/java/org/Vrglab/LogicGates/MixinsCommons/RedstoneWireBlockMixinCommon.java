29a5aa5683f65df717266c0a965fce17f675c2cd
Vrglab <vrglab1@gmail.com>
2024-12-30 23:22:43 +0100

    Fixed Mixins

---
+++ common/src/main/java/org/Vrglab/LogicGates/MixinsCommons/RedstoneWireBlockMixinCommon.java
@@ -0,0 +1,41 @@
+package org.Vrglab.LogicGates.MixinsCommons;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.ObserverBlock;
+import net.minecraft.world.level.block.RepeaterBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.Vrglab.LogicGates.World.Blocks.Simple.Gates.NandGateBlock;
+import org.Vrglab.LogicGates.World.Blocks.Simple.Gates.NotGateBlock;
+import org.Vrglab.LogicGates.World.Blocks.Simple.LogicGateBasicBlocks;
+import org.Vrglab.Utils.Utils;
+import org.jetbrains.annotations.Nullable;
+
+public class RedstoneWireBlockMixinCommon {
+
+    public static boolean shouldConnectToReplaced(BlockState blockState, @Nullable Direction direction) {
+        if (blockState.is(Blocks.REDSTONE_WIRE)) {
+            return true;
+        } else if (blockState.is(Blocks.REPEATER)) {
+            Direction direction2 = (Direction)blockState.getValue(RepeaterBlock.FACING);
+            return direction2 == direction || direction2.getOpposite() == direction;
+        } else if (blockState.is(Blocks.OBSERVER)) {
+            return direction == blockState.getValue(ObserverBlock.FACING);
+        } else if(blockState.is((NandGateBlock)Utils.convertToMcSafeType(LogicGateBasicBlocks.NAND_GATE_BLOCK))) {
+            Direction BlockFacing = blockState.getValue(NandGateBlock.FACING);
+            Direction right, left;
+            if (BlockFacing.getAxis().isHorizontal()) {
+                right = BlockFacing.getClockWise();
+                left = BlockFacing.getCounterClockWise();
+            } else {
+                right = Direction.EAST;
+                left = Direction.WEST;
+            }
+            return direction == BlockFacing || direction == right || direction == left;
+        }else if(blockState.is((NotGateBlock)Utils.convertToMcSafeType(LogicGateBasicBlocks.NOT_GATE_BLOCK))) {
+            Direction BlockFacing = blockState.getValue(NandGateBlock.FACING);
+            return direction == BlockFacing || direction == BlockFacing.getOpposite();
+        }
+        return blockState.isSignalSource() && direction != null;
+    }
+}
